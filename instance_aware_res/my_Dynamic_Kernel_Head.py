cd670c64bc676c2fc1ca3c234ac6257b140e16d6
sega-hsj <hsj970319@163.com>
2021-04-29 12:43:29 +0800

    Update my_Dynamic_Kernel_Head.py

--- instance_aware_res/my_Dynamic_Kernel_Head.py
+++ instance_aware_res/my_Dynamic_Kernel_Head.py
@@ -12,40 +12,37 @@ def kernel_conv(kernel,encode_feat):
     kernel=kernel.reshape(B,1,C)
     pred=torch.matmul(kernel,encode_feat) #B,1,HW
     pred=pred.reshape(B,1,H,W)
+    return pred
 
-
-class Decouple_Kernel_Head(nn.Module):
-    def __init__(self, cfg, feat_dim=256, kernel_dim=256):
-        super(Decouple_Kernel_Head, self).__init__()
+class Thres_Kernel_Head(nn.Module):
+    def __init__(self, cfg, in_dim=256, out_dim=64, thres=0.4):
+        super(Thres_Kernel_Head, self).__init__()
         norm = cfg.MODEL.POSITION_HEAD.NORM
-
+        self.thres = thres
         self.kernel_trans = nn.Conv2d(256, 64, kernel_size=1)
 
     def generate_kernel(self,pred_region,kernel_weight):
-        # 1,H,W C,H,W
-        if pred_region.sum()==0:
-            return None
-        H,W=kernel_weight.shape[-2:]
-        area=(pred_region==1).reshape(H,W)
-        kernel=kernel_weight[:,area] # C,num
-        kernel=kernel.mean(dim=-1)
-        return kernel
+        C,H,W=kernel_weight.shape
+        kernel_weight=kernel_weight.reshape(C,-1) #C,HW
+        pred_region=pred_region.reshape(-1) # 1,HW
+        
+        keep=pred_region > self.thres
+        if keep.sum()==0:
+            return torch.zeros(C,1,1).cuda()
+        kernel=kernel_weight[:,keep] # C,num
+        kernel=kernel.mean(dim=-1) # C
+        kernel=kernel.reshape(1,C,1,1)
+        kernel=self.kernel_trans(kernel).squeeze(0)
+        return kernel 
 
     def forward(self,pred_region,kernel_weight,encode_feat):
         B,C,H,W=encode_feat.shape
-        encode_feat=encode_feat.reshape(B,C,H*W)
-        preds=[]
+        kernels=[]
         for i in range(B):
-            kernel=self.generate_kernel(pred_region[i],kernel_weight[i]) #1,H,W  C,H,W  = C
-            if kernel is None:
-                preds.append(torch.zeros(1,H,W).cuda())
-            else:
-                kernel=self.kernel_trans(kernel) #1,C,1,1
-                kernel=kernel.reshape(1,1,C)
-                pred=torch.matmul(kernel,encode_feat[i]) #1,1,C * 1,C,HW = 1,1,HW
-                pred=pred.reshape(1,H,W)
-                preds.append(pred)
-        preds=torch.stack(preds)
+            kernel=self.generate_kernel(pred_region[i],kernel_weight[i]) #1,H,W  C,H,W  = 1,C,1,1
+            kernels.append(kernel)
+        kernel=torch.stack(kernels)
+        preds=kernel_conv(kernel,encode_feat)
         return preds
 
 class Dynamic_Kernel_Head(nn.Module):
@@ -104,6 +101,35 @@ class Softmax_Kernel_Head(nn.Module):
         pred=kernel_conv(kernel,encode_feat)
         return pred
 
+class Sigmoid_Kernel_Head(nn.Module):
+    def __init__(self, cfg, input_dim=256, output_dim=64):
+        super(Sigmoid_Kernel_Head, self).__init__()
+        norm = cfg.MODEL.POSITION_HEAD.NORM
+        self.kernel_trans = nn.Conv2d(input_dim, output_dim, kernel_size=1)
+
+    def generate_kernel(self,score,kernel_weight):
+        B,C,H,W=kernel_weight.shape
+
+        score=score.sigmoid() #B,1,H,W
+        score_sum=score.sum()
+        score=score.repeat(1,C,1,1) #B,C,H,W
+        kernel=kernel_weight*score #B,C,H,W
+        kernel=kernel.reshape(B,C,-1) #B,C,HW
+        kernel=kernel.sum(dim=-1) #B,C
+        kernel=kernel/score_sum
+        kernel=kernel.reshape(B,C,1,1)
+        return kernel
+
+    def forward(self,soft_scores,kernel_weights,encode_feat):
+        kernels=[]
+        for soft_score,kernel_weight in zip(soft_scores,kernel_weights):
+            kernel=self.generate_kernel(soft_score,kernel_weight)
+            kernel=self.kernel_trans(kernel)
+            kernels.append(kernel) #B,C,1,1
+        kernels=torch.stack(kernels)
+        kernel=kernels.mean(dim=0)
+        pred=kernel_conv(kernel,encode_feat)
+        return pred
 
 if __name__=="__main__":
     exit(0)
