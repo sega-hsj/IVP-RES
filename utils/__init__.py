dc26b028ac519a82932bd2b91d09d09457465520
sega-hsj <hsj970319@163.com>
2021-08-20 21:43:43 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-# -----------------------------------------------------------------------------
-# Copyright (c) Edgar Andrés Margffoy-Tuay, Emilio Botero and Juan Camilo Pérez
-#
-# Licensed under the terms of the MIT License
-# (see LICENSE for details)
-# -----------------------------------------------------------------------------
 
 """Misc data and other helping utillites."""
 
